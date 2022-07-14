88ceed9696bb69d1320cadefb20612d1a31f5fc7
RITIK KUMAR <ritik805131@gmail.com>
2022-07-14 22:44:34 +0530

    commit

--- gallery/settings.py
+++ gallery/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-b3yydhob3=)-&foi!&t5j6hacjlx=fgyj7691x)s9n(y#9e5q^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['galleryphoto1.herokuapp.com']
 
 
 # Application definition
